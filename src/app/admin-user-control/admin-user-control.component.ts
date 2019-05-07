7ae0523b659c2b9f6e600e40bd2d66d9e0c640bb
DANellis92 <dvdnellis@gmail.com>
2019-05-07 11:56:23 -0400

    master commit

---
+++ src/app/admin-user-control/admin-user-control.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-user-control',
+  templateUrl: './admin-user-control.component.html',
+  styleUrls: ['./admin-user-control.component.css']
+})
+export class AdminUserControlComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SplashComponent } from './splash/splash.component';
+import { AuthComponent } from './auth/auth.component';
+import { UserComponent } from './user/user.component';
+import { UserlogsComponent } from './userlogs/userlogs.component';
+import { CreateLogComponent } from './create-log/create-log.component';
+import { AdminComponent } from './admin/admin.component';
+import { AdminUserControlComponent } from './admin-user-control/admin-user-control.component';
+import { ExpenseDisplayComponent } from './expense-display/expense-display.component';
+import { ExpenseCreateComponent } from './expense-create/expense-create.component';
+import { LogDisplayComponent } from './log-display/log-display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SplashComponent,
+    AuthComponent,
+    UserComponent,
+    UserlogsComponent,
+    CreateLogComponent,
+    AdminComponent,
+    AdminUserControlComponent,
+    ExpenseDisplayComponent,
+    ExpenseCreateComponent,
+    LogDisplayComponent
   ],
   imports: [
     BrowserModule,
