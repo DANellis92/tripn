949803b4f93a825a313a1676eff82efae3a19c08
Aaron <aaronwebster88@gmail.com>
2019-05-10 09:36:43 -0400

    Material Setup and implementation, CSS variables, Form validations, Reactive Forms setup, pre-merge

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SplashComponent } from './splash/splash.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SplashComponent},
+  { path: 'dashboard', component: UserDashboardComponent},
+  { path: 'admindashboard', component: AdminDashboardComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
