bd2f5c047502a0ab84220499a9583e90e3f4829e
Scott <scott_hoffmann1879@outlook.com>
2019-05-10 09:32:10 -0400

    guards somewhat implemented, pre merge

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, CanActivate } from '@angular/router';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { AdminUserDisplayComponent } from './admin-user-display/admin-user-display.component';
+import { SplashComponent } from './splash/splash.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+import { TripDisplayComponent } from './trip-display/trip-display.component';
+import { TripCreateComponent } from './trip-create/trip-create.component';
+import { ExpenseDisplayComponent } from './expense-display/expense-display.component';
+import { ExpenseCreateComponent } from './expense-create/expense-create.component';
+import { AuthGuardService as AuthGuard } from './auth-guard.service';
+ 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SplashComponent},
+  { path: 'dashboard', component: UserDashboardComponent, canActivate: [AuthGuard]},
+  { path: 'admindashboard', component: AdminDashboardComponent},
+  { path: '**', redirectTo: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
