6bda2ecdef1ea35913ff48475155e4890ba5d1d3
DANellis92 <dvdnellis@gmail.com>
2019-05-15 16:51:12 -0400

    working on admin dash

--- src/app/trip-create/trip-create.component.ts
+++ src/app/trip-create/trip-create.component.ts
@@ -1,76 +1,88 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { MatDialog } from '@angular/material';
-import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
-import { TripService } from '../trip.service';
+import { Component, OnInit, Input } from "@angular/core";
+import { MatDialog } from "@angular/material";
+import {
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators
+} from "@angular/forms";
+import { TripService } from "../trip.service";
 
 @Component({
-  selector: 'app-trip-create',
-  templateUrl: './trip-create.component.html',
-  styleUrls: ['./trip-create.component.css']
+  selector: "app-trip-create",
+  templateUrl: "./trip-create.component.html",
+  styleUrls: ["./trip-create.component.css"]
 })
 export class TripCreateComponent implements OnInit {
+  constructor(public dialog: MatDialog) {}
 
-  constructor(public dialog: MatDialog) { }
+  openDialog() {
+    const dialogRef = this.dialog.open(TripCreateDialog, {
+      height: "80vh",
+      width: "90vw"
+    });
 
-    openDialog() {
-      const dialogRef = this.dialog.open(TripCreateDialog, {
-        height: '80vh',
-        width: '90vw'
-      });
-
-      dialogRef.afterClosed().subscribe(result => {
-        console.log(`Dialog result: ${result}`);
-      });
-    }
-
-  ngOnInit() {
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
   }
+
+  ngOnInit() {}
 }
 
 @Component({
-  selector: 'trip-create-dialog',
-  templateUrl: 'trip-create-dialog.html',
+  selector: "trip-create-dialog",
+  templateUrl: "trip-create-dialog.html"
 })
 export class TripCreateDialog {
-  tripCreate: FormGroup
-  tripname = new FormControl('', [Validators.required]);
-  startdate = new FormControl('', [Validators.required]);
-  enddate = new FormControl('');
-  miles = new FormControl('', [Validators.required]);
+  tripCreate: FormGroup;
+  tripName = new FormControl("", [Validators.required]);
+  startDate = new FormControl("", [Validators.required]);
+  endDate = new FormControl("");
+  distance = new FormControl("", [Validators.required]);
   trip = [];
   @Input("sessionToken") sessionToken: string;
 
-  constructor(public dialog: MatDialog, private fb: FormBuilder, private tripService: TripService) { 
+  constructor(
+    public dialog: MatDialog,
+    private fb: FormBuilder,
+    private tripService: TripService
+  ) {
     this.tripCreate = fb.group({
       hideRequired: false,
-      floatLabel: 'auto'
+      floatLabel: "auto"
     });
   }
 
   ngOnInit() {
     this.tripCreate = this.fb.group({
-      tripname : new FormControl(null, [Validators.required]),
-      startdate : new FormControl(null, [Validators.required]),
-      enddate : new FormControl(null),
-      miles : new FormControl(null, [Validators.required])
-    })
+      tripName: new FormControl(null, [Validators.required]),
+      startDate: new FormControl(null, [Validators.required]),
+      endDate: new FormControl(""),
+      distance: new FormControl(null, [Validators.required])
+    });
   }
 
-  onCreateTrip() : void {
-    this.tripService.createTrip(this.tripCreate.value, this.sessionToken).subscribe(Trip => this.trip = Trip);
+  onCreateTrip(tripCreate): void {
+    this.tripService
+      .createTrip(tripCreate, this.sessionToken)
+      .subscribe(Trip => (this.trip = Trip));
     console.log(this.tripCreate.value);
   }
 
   getTripnameError() {
-    return this.tripname.hasError('required') ? 'You must enter a name' : null;
+    return this.tripName.hasError("required") ? "You must enter a name" : null;
   }
 
   getStartdateError() {
-    return this.startdate.hasError('required') ? 'You must enter a start date' : null;
+    return this.startDate.hasError("required")
+      ? "You must enter a start date"
+      : null;
   }
 
   getMilesError() {
-    return this.miles.hasError('required') ? 'You must enter a distance' : null;
+    return this.distance.hasError("required")
+      ? "You must enter a distance"
+      : null;
   }
 }
-
