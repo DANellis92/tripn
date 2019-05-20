1c3c20c407a839654432ab9c22d6ccf1f6702581
DANellis92 <dvdnellis@gmail.com>
2019-05-20 13:22:26 -0400

    expense delete built

--- src/app/expense-display/expense-display.component.ts
+++ src/app/expense-display/expense-display.component.ts
@@ -1,38 +1,44 @@
-import { Component, OnInit, Input, Inject, Output, EventEmitter } from '@angular/core';
-import { MatDialog, MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
-import { ExpenseService } from '../expense.service';
+import {
+  Component,
+  OnInit,
+  Input,
+  Inject,
+  Output,
+  EventEmitter
+} from "@angular/core";
+import { MatDialog, MAT_DIALOG_DATA, MatDialogRef } from "@angular/material";
+import { ExpenseService } from "../expense.service";
 
 @Component({
-  selector: 'app-expense-display',
-  templateUrl: './expense-display.component.html',
-  styleUrls: ['./expense-display.component.css']
+  selector: "app-expense-display",
+  templateUrl: "./expense-display.component.html",
+  styleUrls: ["./expense-display.component.css"]
 })
 export class ExpenseDisplayComponent implements OnInit {
   @Input("trip") trip: object;
   @Input("expense") expenses: object;
 
-  constructor(public dialog: MatDialog) { }
+  constructor(public dialog: MatDialog) {}
 
   openDialog() {
     const dialogRef = this.dialog.open(ExpenseDisplayDialog, {
-      height: '80vh',
-      width: '90vw',
+      height: "80vh",
+      width: "90vw",
       data: this.trip,
-      panelClass:"tripn-no-padding-dialog"
+      panelClass: "tripn-no-padding-dialog"
     });
 
     dialogRef.afterClosed().subscribe(result => {
       console.log(`Dialog result: ${result}`);
     });
   }
-  ngOnInit() {
-  }
+  ngOnInit() {}
 }
 
 @Component({
-  selector: 'expense-display-dialog',
-  templateUrl: 'expense-display-dialog.html',
-  styleUrls: ['./expense-display.component.css']
+  selector: "expense-display-dialog",
+  templateUrl: "expense-display-dialog.html",
+  styleUrls: ["./expense-display.component.css"]
 })
 export class ExpenseDisplayDialog {
   expenses: [];
@@ -44,7 +50,6 @@ export class ExpenseDisplayDialog {
     private expenseService: ExpenseService,
     public dialogRef: MatDialogRef<ExpenseDisplayDialog>,
     @Inject(MAT_DIALOG_DATA) public data: any
-
   ) {
     this.userId = sessionStorage.getItem("userId");
   }
@@ -52,18 +57,22 @@ export class ExpenseDisplayDialog {
   @Input("expense") expense: object;
 
   fetchAllExpenses() {
-    this.expenseService.getMyExpenses(this.userId, this.data.id, this.sessionToken).subscribe(expense => {
-      this.expenses = expense
-      this.expenses.reverse();
-      console.log(this.data);
-    })
+    this.expenseService
+      .getMyExpenses(this.userId, this.data.id, this.sessionToken)
+      .subscribe(expense => {
+        this.expenses = expense;
+        this.expenses.reverse();
+        console.log(this.data);
+      });
   }
 
   ngOnInit() {
     this.fetchAllExpenses();
   }
-
-
+  deleteExpense(expense) {
+    console.log(this.data.id, expense.id);
+    let tripId = this.data.id;
+    let id = expense.id;
+    this.expenseService.deleteExpense(tripId, id).subscribe(res => res);
+  }
 }
-
-
